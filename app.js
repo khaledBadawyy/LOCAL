73323533e47430f8dc04c05b9d8c789cd0c4f218
unknown <khaledbadawy315@gmail.com>
2025-03-09 15:14:18 +0200

    last-edit

--- app.js
+++
@@ -1,5 +0,0 @@
-function applyStyle() {
-  document.body.style.backgroundColor =
-    document.body.style.backgroundColor === "lightblue" ? "white" : "lightblue";
-  console.log("تم تغيير لون الخلفية!");
-}
