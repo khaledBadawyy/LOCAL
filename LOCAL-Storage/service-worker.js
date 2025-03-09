5b99015b9eb5aa03b6a1b8b40ab64b9d6b9d1906
unknown <khaledbadawy315@gmail.com>
2025-03-09 15:43:37 +0200

    last-thrree-pm

---
+++ LOCAL-Storage/service-worker.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = "my-cache-v2";
+const urlsToCache = [
+  "index.html",
+  "style.css",
+  "app.js",
+  "newpage.html",
+  " manifest.json",
+  " icon-192x192.png",
+  " icon-512x512.png",
+];
+
+// تثبيت Service Worker
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache).then(() => {
+        console.log("تم تخزين جميع الملفات في الكاش بنجاح!");
+      });
+    })
+  );
+});
+
+// استجابة الطلبات من الكاش
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// تحديث الكاش عند تغيير الملفات
+self.addEventListener("activate", (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      )
+    )
+  );
+});
