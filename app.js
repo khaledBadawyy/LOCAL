aa6062864e7a5d0edad50c95c1b130b75296394e
unknown <khaledbadawy315@gmail.com>
2025-03-11 15:18:55 +0200

    add-new-line

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register("/service-worker.js")
+    navigator.serviceWorker.register("./src/service-worker.js")
+
         .then((registration) => {
             console.log("Service Worker مسجل بنجاح:", registration);
         })
